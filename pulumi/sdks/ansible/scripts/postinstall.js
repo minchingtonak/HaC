66ad4dbb1555e7829bd692b5d3d1556723248f97
Alec Minchington <alec.minch@gmail.com>
2025-10-04 00:26:09 -0700

    chore: update generated sdk files

--- pulumi/sdks/ansible/scripts/postinstall.js
+++ pulumi/sdks/ansible/scripts/postinstall.js
@@ -2,8 +2,13 @@ const fs = require("node:fs");
 const path = require("node:path");
 const process = require("node:process");
 const { execSync } = require('node:child_process');
+// We want to run "tsc --types node --types something-else ..." for each @types package.
+const packageJSON = JSON.parse(fs.readFileSync("package.json") ?? "{}");
+const deps = Object.keys(packageJSON.dependencies ?? []).concat(Object.keys(packageJSON.devDependencies ?? []));
+const types = deps.filter(d => d.startsWith("@types/")).map(d => d.slice("@types/".length)).join(",");
+const typesFlag = types.length > 0 ? " --types " + types : "";
 try {
-  execSync("tsc");
+  execSync("tsc"+typesFlag);
 } catch (error) {
   console.error("Failed to run 'tsc'", {
     stdout: error.stdout.toString(),
